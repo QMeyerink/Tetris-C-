22b416fbb81f1748e8828645fc28911f06942ed3
hic21 <hic21@uclive.ac.nz>
2018-10-10 16:09:23 +1300

    IR communication modules

---
+++ communication.c
@@ -0,0 +1,36 @@
+#include "ir_uart.h"
+
+/* Check if the opponent is already failed and player won. */
+int victory(void)
+{
+    ir_uart_init ();
+    int player_victory = 0;
+
+    while (!lost)
+    {
+        if (ir_uart_read_ready_p ())
+        {
+            char opponent_fail = 0;
+            opponent_fail = ir_uart_getc ();
+            if (opponent_fail = 'F')
+            {
+                player_victory = 1;
+            }
+        }
+    }
+    return player_victory;
+}
+
+
+/* Send to opponent what the player failed. */
+void send_failed(void)
+{
+    ir_uart_init ();
+
+    if (ir_uart_write_ready_p ())
+    {
+        char player_failed = 'F';
+        ir_uart_putc(player_failed);
+    }
+}
+    
---
+++ communication.h
@@ -0,0 +1,10 @@
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+/** Constantly checking if the opponent failed while the player is still playing. */
+int victory(void);
+
+/** Send a character to opponent to tell the player failed. */
+void send_failed(void);
+
+#endif
